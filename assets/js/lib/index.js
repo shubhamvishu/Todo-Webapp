565594c3f017e10c53d2a16a5952d2d77efc88dd
Shubham Chaudhary <shubhmasand@gmail.com>
2020-05-08 20:11:22 +0530

    Basic css styling

--- assets/js/lib/index.js
+++ assets/js/lib/index.js
@@ -1,3 +1,17 @@
-$('li').click(function(){
+$('ul').on("click","li",function(){
     $(this).toggleClass('completed');
+});
+$('ul').on("click","span",function(e){
+    $(this).parent().fadeOut(1000,function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+$("input[type='text']").keypress(function(e){
+    if(e.which===13){
+        console.log("yo");
+        var addtodoText=$("input[type='text']").val();
+        $("<li><span>X</span>"+addtodoText+"</li>").appendTo("#todolist");
+        $(this).val("");
+    }
 });
