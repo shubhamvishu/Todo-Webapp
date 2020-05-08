6963219fa22d856c29b4b1e48eaac90dc1a68173
Shubham Chaudhary <shubhmasand@gmail.com>
2020-05-09 00:09:09 +0530

    Added dark theme to todo list

--- assets/js/lib/index.js
+++ assets/js/lib/index.js
@@ -2,7 +2,7 @@ $('ul').on("click","li",function(){
     $(this).toggleClass('completed');
 });
 $('ul').on("click","span",function(e){
-    $(this).parent().fadeOut(1000,function(){
+    $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
     e.stopPropagation();
@@ -11,7 +11,19 @@ $("input[type='text']").keypress(function(e){
     if(e.which===13){
         console.log("yo");
         var addtodoText=$("input[type='text']").val();
-        $("<li><span>X</span>"+addtodoText+"</li>").appendTo("#todolist");
+        $("<li><span><i class='fa fa-trash'></i></span>"+addtodoText+"</li>").appendTo("#todolist");
         $(this).val("");
     }
 });
+$("#editbutton").click(function(){
+    $("input[type='text']").fadeToggle(500);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'top',
+        hoverEnabled: true
+      });
+  });
+
