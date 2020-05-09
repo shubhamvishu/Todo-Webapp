8c35bf56ef4ce91dda9868a0ebc926d697f98912
Shubham Chaudhary <shubhmasand@gmail.com>
2020-05-09 20:11:45 +0530

    Added Light mode along with dark mode

--- assets/js/lib/index.js
+++ assets/js/lib/index.js
@@ -11,12 +11,32 @@ $("input[type='text']").keypress(function(e){
     if(e.which===13){
         console.log("yo");
         var addtodoText=$("input[type='text']").val();
-        $("<li><span><i class='fa fa-trash'></i></span>"+addtodoText+"</li>").appendTo("#todolist");
+        $("<li><span><i id='delbutton' class='material-icons'>delete</i></span>"+addtodoText+"</li>").appendTo("#todolist");
         $(this).val("");
     }
 });
 $("#editbutton").click(function(){
     $("input[type='text']").fadeToggle(500);
+});
+
+$("#change-theme").on("click",function(){
+    console.log("yayy");
+    $("html").toggleClass("toggle-mode");
+    if($(this).text()==="Light Theme"){
+        $(this).text("Dark Theme");
+        $("#todolist li").removeClass("todo-list");
+        $("#todolist li").toggleClass("toggle-mode-li");
+        $("#addtodoinput").removeClass("todo-add-text");
+        $("#addtodoinput").toggleClass("toggle-todo-add-text");
+    } 
+    else {
+        $(this).text("Light Theme");
+        $("#todolist li").addClass("todo-list");
+        $("#todolist li").toggleClass("toggle-mode-li");
+        $("#addtodoinput").addClass("todo-add-text");
+        $("#addtodoinput").toggleClass("toggle-todo-add-text");
+    }
+    
 });
 
 document.addEventListener('DOMContentLoaded', function() {
