7cedbdc3cc5e2fa80021f9fe4e67a519b45cc547
Shubham Chaudhary <shubhmasand@gmail.com>
2020-05-09 22:13:30 +0530

    Todo webapp offline with light and dark mode

--- assets/js/lib/index.js
+++ assets/js/lib/index.js
@@ -12,6 +12,17 @@ $("input[type='text']").keypress(function(e){
         console.log("yo");
         var addtodoText=$("input[type='text']").val();
         $("<li><span><i id='delbutton' class='material-icons'>delete</i></span>"+addtodoText+"</li>").appendTo("#todolist");
+        if($("#change-theme").text()==="Light Theme"){
+            console.log("1");
+            $("#todolist li").last().addClass("todo-list");
+            //$("#todolist li").last().addClass("toggle-mode-li");
+        }
+        else{
+            console.log($("#todolist li").last());
+            $("#todolist li").last().addClass("toggle-mode-li");
+        }
+        //$("#todolist").last().toggleClass("todo-list");
+        //$("#todolist").last().toggleClass("toggle-todo-list");
         $(this).val("");
     }
 });
@@ -25,16 +36,16 @@ $("#change-theme").on("click",function(){
     if($(this).text()==="Light Theme"){
         $(this).text("Dark Theme");
         $("#todolist li").removeClass("todo-list");
-        $("#todolist li").toggleClass("toggle-mode-li");
+        $("#todolist li").addClass("toggle-mode-li");
         $("#addtodoinput").removeClass("todo-add-text");
-        $("#addtodoinput").toggleClass("toggle-todo-add-text");
+        $("#addtodoinput").addClass("toggle-todo-add-text");
     } 
     else {
         $(this).text("Light Theme");
         $("#todolist li").addClass("todo-list");
-        $("#todolist li").toggleClass("toggle-mode-li");
+        $("#todolist li").removeClass("toggle-mode-li");
         $("#addtodoinput").addClass("todo-add-text");
-        $("#addtodoinput").toggleClass("toggle-todo-add-text");
+        $("#addtodoinput").removeClass("toggle-todo-add-text");
     }
     
 });
